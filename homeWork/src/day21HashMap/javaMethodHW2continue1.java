9e88a185e68e91a84bc550b15267e7c884067ba3
Meryemneslih <mnesli@gmail.com>
2019-11-07 11:04:12 -0500

    Home Work Nov.5

---
+++ homeWork/src/day21HashMap/javaMethodHW2continue1.java
@@ -0,0 +1,16 @@
+package day21HashMap;
+
+public class javaMethodHW2continue1 {
+    public static void main(String[] args) {
+        class str{
+            str b= new str();
+
+
+
+
+        }
+
+
+
+    }
+}
